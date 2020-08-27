ce32ced480ab70500506556afa2ffcd963923fe2
Giovanna Teodoro <giovanna.teod@gmail.com>
2020-08-27 19:24:26 -0400

    Rename example.mp4

--- script_install_and_test.py
+++ script_install_and_test.py
@@ -5,7 +5,7 @@ import time
 
 def record_screen():
     print("GRAVANDO TELA")
-    output = subprocess.Popen("adb shell screenrecord --time-limit 20 /sdcard/DCIM/example.mp4", shell=True, stdout=subprocess.PIPE)
+    subprocess.Popen("adb shell screenrecord --time-limit 20 /sdcard/DCIM/install_and_test.mp4", shell=True, stdout=subprocess.PIPE)
     time.sleep(3)
 
 def get_devices():
