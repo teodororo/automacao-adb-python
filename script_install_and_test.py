339fafab1dcb97321168a4448aba227e7849bd7c
Giovanna Teodoro <giovanna.teod@gmail.com>
2020-08-27 18:37:39 -0400

    Optimizes install_and_test

--- script_install_and_test.py
+++ script_install_and_test.py
@@ -3,6 +3,11 @@ import xml.etree.ElementTree as ET
 import time
 
 
+def record_screen():
+    print("GRAVANDO TELA")
+    output = subprocess.Popen("adb shell screenrecord --time-limit 20 /sdcard/DCIM/example.mp4", shell=True, stdout=subprocess.PIPE)
+    time.sleep(3)
+
 def get_devices():
     output = subprocess.Popen('adb devices', shell=True, stdout=subprocess.PIPE).communicate()[0]
     output = str(output).split('attached')[1]
@@ -10,32 +15,27 @@ def get_devices():
     output = output.replace("\\n", "").replace("\\r", "")
     output = output.replace("\\t", "")
     return output
-
-'''     GRAVAR A TELA (VARIACAO DO PRINT)     
-def record_screen():
-    print("GRAVANDO TELA")
-    output = subprocess.Popen("adb shell screenrecord --time-limit 20 /sdcard/DCIM/example.mp4", shell=True, stdout=subprocess.PIPE)
-
+  
 def get_screen(serial):
+    print("CRIANDO WINDOW_DUMP.XML")
     output = subprocess.Popen('adb -s %s shell uiautomator dump'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
     print(output)
     output = subprocess.Popen('adb -s %s pull /sdcard/window_dump.xml'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
     print(output)
 
+
 def tap_screen(app_name, x, y):
-    print("Abrindo %s" % app_name)
+    print("ABRINDO %s" % app_name)
     output = subprocess.Popen("adb shell input tap %s %s" % (x, y), shell=True, stdout=subprocess.PIPE)
-    time.sleep(3)
-    operation()
     time.sleep(5)
-    print("Voltando a tela anterior")
-    output = subprocess.Popen("adb shell input keyevent 4", shell=True, stdout=subprocess.PIPE)
+    print("VOLTANDO A TELA INICIAL")
+    output = subprocess.Popen("adb shell input keyevent 3", shell=True, stdout=subprocess.PIPE)
 
 def test_app(node, app_name):
     if node.attrib.get('text') == app_name:
         position = node.attrib.get('bounds').split(']')[0]
         position = position.replace("[", "").split(",")
-        print("Testando APP")
+        print("TESTANDO APP")
         tap_screen(app_name, position[0], position[1])
     else:
         for n in node.findall('node'):
@@ -45,25 +45,32 @@ def get_apps(name_app):
     xml = ET.parse('window_dump.xml')
     root = xml.getroot()
     app = test_app(root, name_app)
-'''
+
 def install_apps():
+    print("INSTALANDO APK")
     swipe_screen()
-    output = subprocess.Popen("adb install 'Área de Trabalho'/teste/apks/wikipedia.apk", shell=True, stdout=subprocess.PIPE)
-    output = subprocess.Popen("adb install 'Área de Trabalho'/teste/apks/ankidroid.apk", shell=True, stdout=subprocess.PIPE)
+    output = subprocess.Popen("adb install ./apks/wikipedia.apk", shell=True, stdout=subprocess.PIPE)
+   # output = subprocess.Popen("adb install ./apks/ankidroid.apk", shell=True, stdout=subprocess.PIPE)
     time.sleep(5)
-    output = subprocess.Popen("adb shell input keyevent 4", shell=True, stdout=subprocess.PIPE)
+   # output = subprocess.Popen("adb shell input keyevent 4", shell=True, stdout=subprocess.PIPE)
 
 def swipe_screen():
     output = subprocess.Popen("adb shell input swipe 300 2000 300 500 200", shell=True, stdout=subprocess.PIPE)
     time.sleep(3)
 
+def get_record():
+    time.sleep(10)
+    print("PASSANDO ARQUIVO GRAVADO PARA COMPUTADOR")
+    output = subprocess.Popen("adb shell pull /sdcard/DCIM/example.mp4 .", shell=True, stdout=subprocess.PIPE)
 
 device = get_devices() # passo 1 no original
-
+record_screen()
 install_apps()
-#record_screen()
-#get_screen(device) # passo 2 no original
-#get_apps('Wikipédia') # passo 3 no original
+# swipe_screen()
+get_screen(device) # passo 2 no original
+get_apps('Wikipédia') # passo 3 no original
+get_record()
+
 #get_apps('AnkiDroid')
 
 
