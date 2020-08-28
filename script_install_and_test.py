35ca00c3e3eafe63a84168372b7f6baaa511310d
guilherme-gomes01 <guilhermejgsg01@gmail.com>
2020-08-27 20:32:15 -0400

    add uninstall_apps function to the script_install_and_test file

--- script_install_and_test.py
+++ script_install_and_test.py
@@ -5,7 +5,7 @@ import time
 
 def record_screen():
     print("GRAVANDO TELA")
-    subprocess.Popen("adb shell screenrecord --time-limit 20 /sdcard/DCIM/install_and_test.mp4", shell=True, stdout=subprocess.PIPE)
+    subprocess.Popen("adb shell screenrecord --time-limit 30 /sdcard/DCIM/install_and_test.mp4", shell=True, stdout=subprocess.PIPE)
     time.sleep(3)
 
 def get_devices():
@@ -50,9 +50,16 @@ def install_apps():
     print("INSTALANDO APK")
     swipe_screen()
     output = subprocess.Popen("adb install ./apks/wikipedia.apk", shell=True, stdout=subprocess.PIPE)
-   # output = subprocess.Popen("adb install ./apks/ankidroid.apk", shell=True, stdout=subprocess.PIPE)
-    time.sleep(5)
-   # output = subprocess.Popen("adb shell input keyevent 4", shell=True, stdout=subprocess.PIPE)
+    # output = subprocess.Popen("adb install ./apks/ankidroid.apk", shell=True, stdout=subprocess.PIPE)
+    time.sleep(3)
+
+def uninstall_apps():
+    print("DESINSTALANDO APK")
+    swipe_screen()
+    time.sleep(2)
+    output = subprocess.Popen("adb uninstall org.wikipedia", shell=True, stdout=subprocess.PIPE)
+    time.sleep(2)
+    output = subprocess.Popen("adb shell input keyevent 4", shell=True, stdout=subprocess.PIPE)
 
 def swipe_screen():
     output = subprocess.Popen("adb shell input swipe 300 2000 300 500 200", shell=True, stdout=subprocess.PIPE)
@@ -63,37 +70,14 @@ def get_record():
     print("PASSANDO ARQUIVO GRAVADO PARA COMPUTADOR")
     output = subprocess.Popen("adb shell pull /sdcard/DCIM/example.mp4 .", shell=True, stdout=subprocess.PIPE)
 
-device = get_devices() # passo 1 no original
+device = get_devices()
 record_screen()
 install_apps()
-# swipe_screen()
-get_screen(device) # passo 2 no original
-get_apps('Wikipédia') # passo 3 no original
+get_screen(device)
+get_apps('Wikipédia')
+uninstall_apps()
 get_record()
 
 #get_apps('AnkiDroid')
 
 
-
-
-
-# open_app() >>>>>>>>>>> CHAMADA DA FUNÇÃO
-'''
--------------- ABRINDO APLICATIVO POR PACOTE --------------
-def open_app():
-    output = subprocess.Popen("adb shell monkey -p com.android.chrome 1", shell=True, stdout=subprocess.PIPE)
-    text()
-    
--------------- FUNÇÃO OPERATION (REALIZA PESQUISA NO CHROME) --------------
-def operation():
-    #get_screen(device)
-    output = subprocess.Popen("adb shell input tap 77 630", shell=True, stdout=subprocess.PIPE)
-    time.sleep(1)
-    search = "hefesto%suea"
-    output = subprocess.Popen("adb shell input text %s" %search, shell=True, stdout=subprocess.PIPE)
-    time.sleep(1)
-    output = subprocess.Popen("adb shell input keyevent 66", shell=True, stdout=subprocess.PIPE)
-    time.sleep(2)
-    output = subprocess.Popen("adb shell input swipe 300 2000 300 500", shell=True, stdout=subprocess.PIPE)
-    time.sleep(1)
-'''
