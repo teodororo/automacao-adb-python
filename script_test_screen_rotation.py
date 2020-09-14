2eb7fc6226fb0e10cc5e09bd53e6bca0d7ee8c56
Giovanna Teodoro <giovanna.teod@gmail.com>
2020-09-14 17:46:28 -0400

    Fix some mistakes

--- script_test_screen_rotation.py
+++ script_test_screen_rotation.py
@@ -3,8 +3,8 @@ import xml.etree.ElementTree as ET
 import time
 
 def record_screen():
-    print("GRAVANDO TELA")
-    output = subprocess.Popen("adb shell screenrecord --time-limit 7 /sdcard/DCIM/screen_rotation.mp4", shell=True, stdout=subprocess.PIPE)
+    print("RECORDING THE SCREEN")
+    subprocess.Popen("adb shell screenrecord --time-limit 7 /sdcard/DCIM/screen_rotation.mp4", shell=True, stdout=subprocess.PIPE)
     time.sleep(3)
 
 def get_devices():
@@ -16,22 +16,20 @@ def get_devices():
     return output
 
 def get_screen(serial):
-    print("CRIANDO WINDOW_DUMP.XML")
-    output = subprocess.Popen('adb -s %s shell uiautomator dump'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
-    print(output)
-    output = subprocess.Popen('adb -s %s pull /sdcard/window_dump.xml'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
-    print(output)
+    print("CREATING WINDOW_DUMP.XML")
+    subprocess.Popen('adb -s %s shell uiautomator dump'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
+    subprocess.Popen('adb -s %s pull /sdcard/window_dump.xml'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
 
 def tap_screen(app_name, x, y):
-    print("ABRINDO %s" % app_name)
-    output = subprocess.Popen("adb shell input tap %s %s" % (x, y), shell=True, stdout=subprocess.PIPE)
+    print("OPENING %s" % app_name)
+    subprocess.Popen("adb shell input tap %s %s" % (x, y), shell=True, stdout=subprocess.PIPE)
     time.sleep(3)
 
 def test_app(node, app_name):
     if node.attrib.get('text') == app_name:
         position = node.attrib.get('bounds').split(']')[0]
         position = position.replace("[", "").split(",")
-        print("TESTANDO APP")
+        print("TESTING APP")
         tap_screen(app_name, position[0], position[1])
     else:
         for n in node.findall('node'):
@@ -44,22 +42,22 @@ def get_apps(name_app):
 
 def screen_rotation(serial):
     print("TURNING OFF AUTOMATIC SCREEN ROTATION")
-    output = subprocess.Popen('adb -s %s shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
+    subprocess.Popen('adb -s %s shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
     print("LANDSCAPE MODE")
-    output = subprocess.Popen('adb -s %s shell content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:1'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
+    subprocess.Popen('adb -s %s shell content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:1'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
     time.sleep(3)
     print("PORTRAIT MODE")
-    output = subprocess.Popen('adb -s %s shell content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:0'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
+    subprocess.Popen('adb -s %s shell content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:0'%serial,shell=True,stdout=subprocess.PIPE).communicate()[0]
     time.sleep(3)
     print("CHANGING TO HOME")
-    output = subprocess.Popen("adb shell input keyevent 3", shell=True, stdout=subprocess.PIPE)
+    subprocess.Popen("adb shell input keyevent 3", shell=True, stdout=subprocess.PIPE)
     time.sleep(3)
 
 
 def get_record(serial):
     time.sleep(5)
-    print("PASSANDO ARQUIVO GRAVADO PARA COMPUTADOR")
-    output = subprocess.Popen('adb -s %s pull /sdcard/DCIM/screen_rotation.mp4'%serial, shell=True, stdout=subprocess.PIPE)
+    print("PULLING .mp4")
+    subprocess.Popen('adb -s %s pull /sdcard/DCIM/screen_rotation.mp4'%serial, shell=True, stdout=subprocess.PIPE)
 
 record_screen()
 device = get_devices()
